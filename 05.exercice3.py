f90ce10aaab2e1f652f73844522c0d512f3313f6
Joshered <charlesyered4@gmail.com>
2025-04-18 11:32:58 +0200

    exercice4

---
+++ 05.exercice3.py
@@ -0,0 +1,8 @@
+nom="yered"
+age=22
+taille=1.8
+est_etudiant= True
+print(f"{nom} est du type {type(nom)}")
+print(f"{age} est du type {type(age)}")
+print(f"{taille} est du type {type(taille)}")
+print(f"{est_etudiant} est du type {type(est_etudiant)}")
