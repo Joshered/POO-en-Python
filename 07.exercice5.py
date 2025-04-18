ae1f6c5dee277217fcb40a65eb420f3e36fea13f
Joshered <charlesyered4@gmail.com>
2025-04-18 12:40:15 +0200

    07.exercice5.py

--- 07.exercice5.py
+++ 07.exercice5.py
@@ -1,10 +1,21 @@
+#cree dictionaire
 fruits={
     "pomme":"rouge",
     "banane":"jaune",
     "orange":"orange" 
 }
+
+#ajouter 
 fruits["kiwi"]="vert"
+
+#stocker dans une autre variable
 couleurBanane=fruits["banane"]
+
+#modifier
 fruits["pomme"]="vert"
+
+#dupprimer
 del fruits["banane"]
+
+#afficher
 print(fruits)
