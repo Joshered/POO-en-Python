aa9a44070296cf9acfc1a9080e8eeb73c2ee98e8
Joshered <charlesyered4@gmail.com>
2025-04-18 11:05:48 +0200

    variable

--- 02.variable.py
+++ 02.variable.py
@@ -1,11 +1,3 @@
-def explication():
-    #premier variable
-    num1=5
-    #deuxiemme variable
-    num2=10
-    #changer le nom de la variable
-    num1=7
-    #afficher le contenu du livre
-    print(num1)
+
 
 
