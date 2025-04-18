0f727fc4a6a410b1443bacc197604b8380dcdc32
Joshered <charlesyered4@gmail.com>
2025-04-18 11:40:32 +0200

    exercice4

---
+++ 06.exercice4.py
@@ -0,0 +1,7 @@
+fruits=["pomme","banane","orange"]
+fruits.append("kiwi")
+fruits.remove("orage")
+fruits[1]="ananas"
+print(f"la longeure de la list est{len(fruits)}")
+fruits.sort()
+print(fruits)
