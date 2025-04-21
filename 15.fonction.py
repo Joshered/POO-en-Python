211689d7e49fcb1e76b9eb63e0622e7128948f50
Joshered <charlesyered4@gmail.com>
2025-04-21 12:35:12 +0200

    fonction

--- 15.fonction.py
+++ 15.fonction.py
@@ -4,4 +4,20 @@ def afficher_fichier():
     print("bonjour, comment ça va")
 afficher_fichier()
 '''
+#fonction avec commentaire
+'''
+def afficher_prenom(nom,prenom):
+    print("nom:",nom)
+    print("prenom:",prenom)
+
+afficher_prenom("josh","yered")
+'''
+
+#fonction qui retourne
+def calculer_somme(a,b):
+    resultat = a+b
+    return resultat
+
+somme= calculer_somme(2,3)
+print(somme)
 
