e56cd6f17c018d0c84b3f3f4f25ca2c8c150372c
Joshered <charlesyered4@gmail.com>
2025-04-21 11:54:01 +0200

    Boucle while

--- 12.Boucle.py
+++ 12.Boucle.py
@@ -13,3 +13,21 @@ noms = "Ordinateur"
 for nom in noms :
     print(nom)
 '''
+
+#For range
+'''
+for nom in range(10) :
+    print(f"{nom} bouteilles de bierres au mur!")
+
+#exercices avec valeur initiale
+for nom in range(1,10) :
+    print(f"{nom} bouteilles de bierres au mur!")
+'''
+
+#boucle while
+capacite_maximale = 10
+capacite_actualle = 3
+
+while capacite_actualle < capacite_maximale:
+    print(f"la capacite actuelle est {capacite_actualle}")
+    capacite_actualle +=1
