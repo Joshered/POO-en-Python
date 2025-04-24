58e1c5ee1d7bf6da4fa64788b5b4592b0ce642b6
Joshered <charlesyered4@gmail.com>
2025-04-24 11:43:37 +0200

    triangle

---
+++ 22.Drink.py
@@ -0,0 +1,20 @@
+class Drink:
+    """une boisson"""
+    def __init__(self,price):
+        self.price = price
+        
+    def drink(self):
+        print("Je ne sais pas ce que c'est, mais je le bois")
+        
+class Coffee(Drink):
+    prices={"simple":1,"serre":1,"allongé":1.5}
+    
+    def __init__(self, price):
+        self.type=type
+        super().__init__(price=self.prices.get(type,1))
+        
+    def drink(self):
+        print(f"Un bon café de {self.type} qui coute {self.price}$ pour me reveiller !")
+        
+coffee = Coffee("allonge")
+boire= Coffee.drink()
