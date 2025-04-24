aba360ef70e5cad527b57017573daddd1296b8ab
Joshered <charlesyered4@gmail.com>
2025-04-24 16:22:05 +0200

    stagiaire.py

---
+++ 24.stagiaire.py
@@ -0,0 +1,12 @@
+class Stagiaire:
+    def __init__(self,numi,age,nom,prenom,note1,note2):
+        self.num=numi
+        self.__age=qge
+        self.__nom=nom
+        self.__prenom=prenom
+        self.__note1=note1
+        self.__note2=note2
+        
+    def calculMoyenne(self):
+        moyenne=(self,__note1+self.__note2)/2
+        print("La moyenne:",moyenne)
